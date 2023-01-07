a7a6411a33cad283e7ed387c8e5f8463d695e66d
BhaskarJSharma <bhaskarj.sharma@bcplindia.co.in>
2023-01-07 16:37:19 +0530

    Add project files.

---
+++ Controllers/UserAccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace core_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserAccountController : ControllerBase
+    {
+        [HttpPost]
+        public string RegisterUser(string name, string email,string password)
+        {
+            return "";
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace core_backend.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
